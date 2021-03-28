0765c5ab6d078496e523c75c0db5810903614793
Felipe Zuntini <felipezuntini@hotmail.com>
2021-03-28 00:17:40 -0300

    :zap: (Questions) Adding more spicy questions

--- src/store/state/questions.js
+++ src/store/state/questions.js
@@ -72,5 +72,92 @@ export default [
 				}
 			}
 		]
+	},
+	{
+		text: 'What did Seto Kaiba wanted to see on Yugi\'s grampa shop?',
+		answers: [
+			{
+				text: 'A blue eyes white supremacist dragon card',
+				points: {
+					typeA: -1,
+					typeB: -2,
+					typeC: -3,
+					typeD: -5
+				}
+			},
+			{
+				text: 'A red eyed ganja black dragon',
+				points: {
+					typeC: 3,
+					typeD: 2
+				}
+			},
+			{
+				text: '"O MACACO"',
+				points: {
+					typeA: 3,
+					typeB: 4,
+					typeC: 5,
+					typeD: 2
+				}
+			},
+			{
+				text: 'Pharaoh Anthem',
+				points: {
+					typeC: 1,
+					typeD: 1
+				}
+			}
+		]
+	},
+	{
+		text: 'What type do you think you are?',
+		answers: [
+			{
+				text: 'A Quick Learner',
+				points: {
+					typeA: 10,
+					typeB: -15,
+					typeC: -15,
+					typeD: -15
+				}
+			},
+			{
+				text: 'A Speedster',
+				points: {
+					typeA: -15,
+					typeB: 10,
+					typeC: -15,
+					typeD: -15
+				}
+			},
+			{
+				text: 'A Cool Guy',
+				points: {
+					typeA: -15,
+					typeB: -15,
+					typeC: 10,
+					typeD: -15
+				}
+			},
+			{
+				text: 'A pretty face',
+				points: {
+					typeA: -15,
+					typeB: -15,
+					typeC: -15,
+					typeD: 10
+				}
+			},
+			{
+				text: 'Don\'t cheat me!',
+				points: {
+					typeA: 0,
+					typeB: 0,
+					typeC: 0,
+					typeD: 0
+				}
+			}
+		]
 	}
 ];
